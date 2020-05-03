deecf55a7f0db165b8d10232295f789c20f37945
vivianlsong <63174230+vivianlsong@users.noreply.github.com>
2020-05-03 00:52:57 -0700

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -59,9 +59,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+    byeSpeaker.speakWord(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+    helloSpeaker.speakWord(names[i]);
   }
 }
 
